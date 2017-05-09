129364f5a3511a934f7254110c16957ebea27b63
Tom K Liang <Tom K LIANG/HSDC/HSBC>
2017-05-09 18:24:29 +0800

    Add helm-swoop, swiper, sqlplus to config

--- personal/packages.el
+++ personal/packages.el
@@ -3,6 +3,19 @@
 (add-hook 'clojure-mode-hook (lambda () (enable-paredit-mode)))
 (add-hook 'emacs-lisp-mode-hook (lambda () (enable-paredit-mode)))
 
+;; swiper setting
+(prelude-require-package 'swiper)
+(global-set-key "\C-s" 'swiper)
+
+;; helm swoop
+(prelude-require-package 'helm-swoop)
+
+;; avy char jump
+(prelude-require-package 'avy)
+(global-set-key (kbd "C-:") 'avy-goto-char)
+
+(prelude-require-package 'sqlplus)
+
 ;;; set default shell to powershell
 (setq explicit-shell-file-name "powershell"
       explicit-powershell-args '("-file" "-"))
--- personal/preload/customization.el
+++ personal/preload/customization.el
@@ -1,9 +1,2 @@
-;; set up the proxy
-(setq url-proxy-services '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-                           ("http" . "intpxy1.hk.hsbc:8080")
-                           ("https" . "intpxy1.hk.hsbc:8080")))
-
-;; use base64 encoding for login credentials
-;; to be changed when domain pass is changed
-(setq url-http-proxy-basic-auth-storage (list (list "intpxy1.hk.hsbc:8080"
-                                                    (cons "Input your LDAP ID" "NDM5OTkxNzQ6eDg4TEswOQ=="))))
+(scroll-bar-mode -1)
+(setq prelude-theme 'leuven)
